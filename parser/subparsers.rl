c39cd6073d9c9cf6a9f969aa0912fef751c4cf70
Bruno Dupuis <bdupuis@lisael.org>
2014-03-01 22:10:23 +0100

    add Any and Until subparsers

--- parser/subparsers.rl
+++ parser/subparsers.rl
@@ -36,6 +36,108 @@ func NonBlankFactory([]string) (Subparser, error){
     return NonBlankParser, nil
 }
 
+/////////////// Any
+func AnyFactory(args []string) (Subparser, error){
+    if len(args) == 0 {
+        d := new(DeferredFactoryDef)
+        d.Fact = AnyFactory
+        return nil, d
+    }
+    next_txt := []rune(args[1])
+    next_eof := len(next_txt)
+	return func(data []rune, p *Parser)([]rune, error){
+        idx := p.idx
+        end := p.idx
+        next_idx := 0
+        var next_char rune
+start:
+        if idx == p.eof{ goto break_ }
+        next_char = next_txt[next_idx]
+        switch data[idx] {
+        case next_char:
+            end = idx - next_idx
+            next_idx++
+            if next_idx == next_eof{ goto break_ }
+            idx ++
+            goto start
+        default:
+            idx ++
+            end = idx
+            next_idx = 0
+            goto start
+        }
+break_:
+        ret := data[p.idx: end]
+        p.idx = end
+        return ret, nil
+    }, nil
+
+    return nil, nil
+}
+
+////////////// until
+func UntilFactory(args []string) (Subparser, error){
+    next_txt := []rune(args[0])
+    next_eof := len(next_txt)
+    include := args[1] == "true"
+    // almost same code as `any`, but to improve perfs avoiding tests
+    // we copy/paste it 
+    if !include {
+        return func(data []rune, p *Parser)([]rune, error){
+            start := p.idx
+            end := p.idx
+            next_idx := 0
+            var next_char rune
+start:
+            // may be an error here...
+            if p.idx == p.eof{ goto break_ }
+            next_char = next_txt[next_idx]
+            switch data[p.idx] {
+            case next_char:
+                end = p.idx - next_idx
+                next_idx++
+                if next_idx == next_eof{ goto break_ }
+                p.idx ++
+                goto start
+            default:
+                p.idx ++
+                end = p.idx
+                next_idx = 0
+                goto start
+            }
+break_:
+            ret := data[start : end]
+            return ret, nil
+        }, nil
+    } else {
+        return func(data []rune, p *Parser)([]rune, error){
+            start := p.idx
+            next_idx := 0
+            var next_char rune
+start:
+            // TODO error...
+            if p.idx == p.eof{ goto break_ }
+            next_char = next_txt[next_idx]
+            switch data[p.idx] {
+            case next_char:
+                next_idx ++
+                p.idx ++
+                if next_idx == next_eof{ goto break_ }
+                goto start
+            default:
+                p.idx ++
+                next_idx = 0
+                goto start
+            }
+break_:
+            ret := data[start : p.idx]
+            return ret, nil
+        }, nil
+    }
+}
+
+
+
 ///////////// IP
 func IPV4Parser(data []rune, pr *Parser) ([]rune, error){
     p := pr.idx
@@ -81,21 +183,6 @@ ret_ip:
 // fake factory, requires no args
 func IPV4Factory(args []string) (Subparser, error){
     return IPV4Parser, nil
-}
-
-/////////////// date
-
-func AnyFactory(args []string) (Subparser, error){
-    if len(args) == 0 {
-        d := new(DeferredFactoryDef)
-        d.Fact = AnyFactory
-        return nil, d
-    }
-    println(len(args))
-    println(args[0])
-    next := []rune(args[0])
-    _ = next
-    return nil, nil
 }
 
 
--- parser/subparsers_test.go
+++ parser/subparsers_test.go
@@ -13,6 +13,7 @@ var p *Parser = NewParser()
 type testCase struct{
     data    []rune
     result  []rune
+    end_idx int
     ok      bool
 }
 
@@ -33,8 +34,9 @@ func PerformSubparserTests(sp Subparser, tests []testCase) []string{
             if string(result) != string(case_.result) {
                 terrors = append( terrors, fmt.Sprintf("case %d: got `%s`. expected `%s`", cid, string(result), string(case_.result)))
             }
-            if p.idx != len(result){
-                terrors = append(terrors, fmt.Sprintf("case %d: bad parser index. got `%d`. expected `%d`", cid, p.idx, len(result)))
+            if case_.end_idx < 0 { case_.end_idx = len(result)}
+            if p.idx != case_.end_idx{
+                terrors = append(terrors, fmt.Sprintf("case %d: bad parser index. got `%d`. expected `%d`", cid, p.idx, case_.end_idx))
             }
         }
     }
@@ -67,21 +69,27 @@ func TestIPV4Parser(t *testing.T) {
     tests := []testCase{
         testCase{ []rune("123.123.123.123"),
                   []rune("123.123.123.123"),
+                  -1,
                   true},
         testCase{ []rune("123.123.123.123 .."),
                   []rune("123.123.123.123"),
+                  -1,
                   true},
         testCase{ []rune("0.0.0.0.."),
                   []rune("0.0.0.0"),
+                  -1,
                   true},
         testCase{ []rune("255.255.0.255"),
                   []rune("255.255.0.255"),
+                  -1,
                   true},
         testCase{ []rune("423.123.123.123"),
                   []rune{},
+                  -1,
                   false},
         testCase{ []rune("123.123.123.423"),
                   []rune{},
+                  -1,
                   false},
     }
     res := PerformSubparserTests(IPV4Parser, tests)
@@ -94,12 +102,15 @@ func TestNonBlankParser(t *testing.T) {
     tests := []testCase{
         testCase{ []rune("hello world"),
                   []rune("hello"),
+                  -1,
                   true},
         testCase{ []rune(" hello world"),
                   []rune(""),
+                  -1,
                   true},
         testCase{ []rune("hello"),
                   []rune("hello"),
+                  -1,
                   true},
     }
     res := PerformSubparserTests(NonBlankParser, tests)
@@ -109,12 +120,85 @@ func TestNonBlankParser(t *testing.T) {
 }
 
 func TestAny(t *testing.T){
+    any, _ := AnyFactory([]string{"", " world"})
+    tests := []testCase{
+        testCase{ []rune("hello my world, bitch"),
+                  []rune("hello my"),
+                  -1,
+                  true},
+        testCase{ []rune(" hello world"),
+                  []rune(" hello"),
+                  -1,
+                  true},
+        testCase{ []rune("hello"),
+                  []rune("hello"),
+                  -1,
+                  true},
+    }
+    res := PerformSubparserTests(any, tests)
+    for _, r := range res{
+        t.Errorf(r)
+    }
 
 }
 
+func TestUntil(t *testing.T){
+    until, _ := UntilFactory([]string{" world", "false"})
+    tests := []testCase{
+        testCase{ []rune("hello my world, bitch"),
+                  []rune("hello my"),
+                  13,
+                  true},
+        testCase{ []rune(" hello world"),
+                  []rune(" hello"),
+                  11,
+                  true},
+        testCase{ []rune("hello"),
+                  []rune("hello"),
+                  -1,
+                  true},
+    }
+    res := PerformSubparserTests(until, tests)
+    for _, r := range res{
+        t.Errorf(r)
+    }
+    until, _ = UntilFactory([]string{" world", "true"})
+    tests = []testCase{
+        testCase{ []rune("hello my world, bitch"),
+                  []rune("hello my world"),
+                  -1,
+                  true},
+        testCase{ []rune(" hello world"),
+                  []rune(" hello world"),
+                  -1,
+                  true},
+        testCase{ []rune("hello"),
+                  []rune("hello"),
+                  -1,
+                  true},
+    }
+    res = PerformSubparserTests(until, tests)
+    for _, r := range res{
+        t.Errorf(r)
+    }
+}
+
+
+
+// Quick and dirty benchs... how do the subparsers compare to regexps ?
+
+func TestAnyParserPerfs(t *testing.T){
+    any_re := "(?P<any>.*) trailing stuff"
+    any, _ := AnyFactory([]string{"", " trailing stuff"})
+    ratio := BenchParserPerfs(any, "some stuff trailing stuff", any_re)
+    // I benched it as ~70x faster than regexp
+    if ratio < 30{
+        t.Errorf("Parser too slow. only %d× faster than regexp.", ratio)
+    }
+}
+
 func TestNonBlankParserPerfs(t *testing.T){
-    nonblank_re := "[ \\t]+"
-    //ipv4_re = "(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}\\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
+    nonblank_re := "[^ \\t]+"
     ratio := BenchParserPerfs(NonBlankParser, "qwerty", nonblank_re)
     // I benched it as 35x faster than regexp
     if ratio < 15{
@@ -127,7 +211,7 @@ func TestIPV4ParserPerfs(t *testing.T){
     // this one is slower
     //ipv4_re = "(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)"
     ratio := BenchParserPerfs(IPV4Parser, "123.123.123.123", ipv4_re)
-    // I benched it as 250x faster than regexp
+    // I benched it as ~300x faster than regexp
     if ratio < 130{
         t.Errorf("Parser too slow. only %d× faster than regexp.", ratio)
     }
